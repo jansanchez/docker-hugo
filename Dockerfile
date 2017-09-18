665ccaf8c246d836717f5caadae378ded60afacb
Jan Sanchez <joejansanchez@gmail.com>
2017-09-17 20:47:29 -0500

    chore Dockerfile: Configuracion inicial

---
+++ Dockerfile
@@ -0,0 +1,28 @@
+FROM alpine:latest
+MAINTAINER Jan Sanchez <joejansanchez@gmail.com>
+
+COPY ./run.sh /tmp/run.sh
+ENV HUGO_VERSION=0.27.1
+ADD https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz /tmp
+RUN tar -xf /tmp/hugo_${HUGO_VERSION}_Linux-64bit.tar.gz -C /tmp \
+    && mkdir -p /usr/local/sbin \
+    && mv /tmp/hugo /usr/local/sbin/hugo \
+    && rm -rf /tmp/hugo_${HUGO_VERSION}_linux_amd64
+
+RUN apk update \
+    && apk upgrade \
+    && apk add --no-cache ca-certificates
+
+RUN mkdir -p /tmp/source
+RUN mkdir -p /tmp/output
+RUN chmod 755 /tmp/run.sh
+
+VOLUME /tmp/source
+VOLUME /tmp/output
+
+WORKDIR /tmp/source
+CMD ["/tmp/run.sh"]
+
+EXPOSE 1313
+
+# Forked from https://github.com/jojomi/docker-hugo 
---
+++ run.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env sh
+
+WATCH="${HUGO_WATCH:=false}"
+SLEEP="${HUGO_REFRESH_TIME:=-1}"
+
+echo "HUGO_WATCH:" $WATCH
+echo "HUGO_REFRESH_TIME:" $HUGO_REFRESH_TIME
+echo "HUGO_THEME:" $HUGO_THEME
+echo "HUGO_BASEURL" $HUGO_BASEURL
+echo "ARGS" $@
+
+HUGO=/usr/local/sbin/hugo
+echo "Hugo path: $HUGO"
+
+while [ true ]
+do
+    if [[ $HUGO_WATCH != 'false' ]]; then
+	    echo "Watching..."
+        $HUGO server --watch=true --source="/tmp/source" --theme="$HUGO_THEME" --destination="/tmp/output" --baseURL="$HUGO_BASEURL" --bind="0.0.0.0" "$@" || exit 1
+    else
+	    echo "Building one time..."
+        $HUGO --source="/tmp/source" --theme="$HUGO_THEME" --destination="/tmp/output" --baseURL="$HUGO_BASEURL" "$@" || exit 1
+    fi
+
+    if [[ $HUGO_REFRESH_TIME == -1 ]]; then
+        exit 0
+    fi
+    echo "Sleeping for $HUGO_REFRESH_TIME seconds..."
+    sleep $SLEEP
+done
+
+# Forked from https://github.com/jojomi/docker-hugo 
